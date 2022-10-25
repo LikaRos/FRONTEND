80803e55a5371c81e755de12115046b08e5f1c2d
Lika <lika.rossinskaya@gmal.com>
2022-10-25 16:33:35 +0300

    template

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import {
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom';
+
+//---------------------------------------------------------------//
+export const App = () => {
+  return <></>;
+};
