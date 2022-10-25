8957d61ff7309c94706bcec3ad4ab9f0ca698160
Lika <lika.rossinskaya@gmal.com>
2022-10-26 00:20:20 +0300

    initial settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import {
-  Navigate,
-  Route,
-  Routes,
-  useNavigate,
-  useSearchParams,
-} from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import {
+//   Navigate,
+//   Route,
+//   Routes,
+//   useNavigate,
+//   useSearchParams,
+// } from 'react-router-dom';
 
 //---------------------------------------------------------------//
 export const App = () => {
