8154211fb0e1adcf37f2db6cda54fa17d4f342f5
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-10-26 13:27:15 +0300

    save

---
+++ src/pages/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Header.module.css';
+//-------------------------------------------------------//
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+				
+      </div>
+    </header>
+  );
+}
