7341e252824dc4f6b40cfaa30987d74e8f16f230
Lika <lika.rossinskaya@gmal.com>
2022-10-31 02:16:06 +0200

    questpag

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -1,14 +1,38 @@
-import QuestionTicket from 'components/QuestionTicket/QuestionTicket';
-import { getRandomQuestions } from 'redux/questions/questions-operations';
+// import QuestionTicket from 'components/QuestionTicket/QuestionTicket';
+import {
+  getRandomQuestions,
+  getAnswers,
+} from 'redux/questions/questions-selectors';
+
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+// import { useDispatch } from 'react-redux';
+
+// import {
+//   addAnswers,
+//   clearAnswers,
+//   clearQuestions,
+// } from 'redux/questions/questions-slice';
 
 export default function QuestionPage() {
+  const questions = useSelector(getRandomQuestions);
+  const answers = useSelector(getAnswers);
   //   const dispatch = useDispatch();
-  //   const testTech = useSelector(getTechTest);
-  //   const theoryTest = useSelector(getTheoryTest);
-  //   const isLogin = useSelector(getIsLoggedIn);
-  //   const handleResult = () => {
-  //     const questions = [];
-  //     dispatch(getRandomQuestions(questions));
-  //   };
-  return <></>;
+
+  useEffect(() => {
+    console.log('questions', questions);
+    console.log('answers', answers);
+  }, [questions, answers]);
 }
+
+// export default function QuestionPage() {
+//   const dispatch = useDispatch();
+//   const testTech = useSelector(getTechTest);
+//   const theoryTest = useSelector(getTheoryTest);
+//   const isLogin = useSelector(getIsLoggedIn);
+//   const handleResult = () => {
+//     const questions = [];
+//     dispatch(getRandomQuestions(questions));
+//   };
+//   return <></>;
+// }
