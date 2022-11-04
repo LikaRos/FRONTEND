bf5f67610c490088bfd2856e5b64c92dd30e4fab
Lika <lika.rossinskaya@gmal.com>
2022-11-04 18:27:08 +0200

    Update MainPage.jsx

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,52 +1,12 @@
 import Main from 'components/Main/Main';
 import QuestionButton from 'components/QuestionButton/QuestionButton';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRandomQuestions } from 'redux/questions/questions-operations';
-// import { Modal } from 'components/AuthForm/AuthModal';
-// import { userGet } from 'redux/Auth/user-operations';
-// import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import { getEmail } from 'redux/Auth/auth-selector';
-// import { useDispatch } from 'react-redux';
-
-// import styles from './MainPage.module.css';
-
-// import { getIsLoggedIn } from 'redux/Auth/auth-selector';
-// import { useDispatch } from 'react-redux';
 
 export default function MainPage() {
   const userEmail = { email: useSelector(getEmail) };
   console.log(userEmail);
-
-  // const [type, setType] = useState(''); //для статистики
-  let type = null;
-  const dispatch = useDispatch();
-  // const [verify, setVerify] = useState(false);
-  // useEffect(() => {
-  //   console.log(dispatch(userGet()));
-  // }, [verify, dispatch]);
-
-  //  const [randomQuestions, setRandomQuestions] = useState([]); //для статистики
-
-  //   const addQuestion = () => {
-  //     setRandomQuestion;
-  //   };
-
-  const handleUpdate = async event => {
-    const { name } = event.target;
-    switch (name) {
-      case 'tech':
-        type = 'tech';
-        break;
-
-      case 'theory':
-        type = 'theory';
-        break;
-
-      default:
-        return;
-    }
-    dispatch(getRandomQuestions(type));
-  };
 
   return (
     <>
