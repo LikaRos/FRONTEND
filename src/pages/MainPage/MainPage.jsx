8a60baf097f8e2a937e765fa50c3abb3fca61146
andrii_h <galgayand@gmail.com>
2022-11-05 20:18:56 +0200

    fix-modal

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,24 +3,31 @@ import QuestionButton from 'components/QuestionButton/QuestionButton';
 import { useEffect } from 'react';
 import { userGet } from 'redux/Auth/user-operations';
 import { useDispatch, useSelector } from 'react-redux';
-import { getToken, getVerify } from '../../redux/Auth/auth-selector';
+import {
+  getToken,
+  getVerify,
+  getLoading,
+} from '../../redux/Auth/auth-selector';
 import { tokenAuth } from '../../services/API';
 import { Modal } from 'components/AuthForm/AuthModal';
 
 export default function MainPage() {
   const token = useSelector(getToken);
   const verify = useSelector(getVerify);
-  console.log(verify);
+  const isLoading = useSelector(getLoading);
+  console.log(isLoading);
   const dispatch = useDispatch();
   useEffect(() => {
     tokenAuth.set(token);
-    dispatch(userGet());
+    if (!verify) {
+      dispatch(userGet());
+    }
   });
   return (
     <>
-      {!verify && <Modal />}
       <Main />
       <QuestionButton />
+      {!verify && !isLoading && <Modal />}
     </>
   );
 }
