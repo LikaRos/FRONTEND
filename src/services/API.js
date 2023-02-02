1187004258e0a7cbc9913f2fa7b17aad6263392c
Lika <lika.rossinskaya@gmail.com>
2023-02-02 15:55:38 +0200

    baseUrl edited

--- src/services/API.js
+++ src/services/API.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 export const API = axios.create({
   //   baseURL: 'http://localhost:3001',
   //   baseURL: 'https://pro-test-team-project.herokuapp.com',
-  baseURL: 'https://teamproject-protest-backend.vercel.app',
+
+  //   baseURL: 'https://teamproject-protest-backend.vercel.app',
+
+  baseURL: 'https://teamproject-protest-backend-impcd6np4-likaros.vercel.app',
 });
 
 export const tokenAuth = {
